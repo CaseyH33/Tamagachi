490e261e2129862b081327d8769bfd420eff8ffa
Mitch Rivet & Casey Heitz <student@epicodus.com>
2015-08-05 16:11:26 -0700

    Adds name form

---
+++ app/app.php
@@ -0,0 +1,29 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Tamagachi.php";
+
+    session_start();
+
+    if (empty($_SESSION['list_of_tamagachis'])) {
+      $_SESSION['list_of_tamagachis'] = array();
+    }
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array (
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app)
+    {
+      return $app['twig']->render('add_tamagachi.html.twig');
+    });
+
+    $app->post("/tamagachi", function() use ($app) {
+      $tamagachi = new Tamagachi($_POST['name']);
+      $tamagachi->save();
+      return $app['twig']->render('tamagachis.html.twig', array('new_tamagachi' => $tamagachi));
+    });
+
+    return $app;
+?>
---
+++ src/Tamagachi.php
@@ -0,0 +1,67 @@
+<?php
+
+  class Tamagachi
+  {
+    private $name;
+    private $food;
+    private $rest;
+    private $attention;
+
+    function __construct($user_name, $user_food = 5, $user_rest = 5, $user_attention = 5)
+    {
+      $this->name = $user_name;
+      $this->food = $user_food;
+      $this->rest = $user_rest;
+      $this->attention = $user_attention;
+    }
+
+    function getName()
+    {
+      return $this->name;
+    }
+
+    function setName($new_name)
+    {
+      $this->name = $new_name;
+    }
+
+    function getFood()
+    {
+      return $this->food;
+    }
+
+    function setFood($new_food)
+    {
+      $this->food = $new_food;
+    }
+
+    function getRest()
+    {
+      return $this->rest;
+    }
+
+    function setRest($new_rest)
+    {
+      $this->rest = $new_rest;
+    }
+
+    function getAttention()
+    {
+      return $this->attention;
+    }
+
+    function setAttention($new_attention)
+    {
+      $this->attention = $new_attention;
+    }
+
+    function save()
+    {
+      array_push($_SESSION['list_of_tamagachis'], $this);
+    }
+
+    static function getAll()
+    {
+      return $_SESSION['list_of_places'];
+    }
+  }
